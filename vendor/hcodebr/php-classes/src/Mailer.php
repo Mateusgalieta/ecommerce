3065537b350695be401c2c006aa546541bb13266
Mateus Galieta <mateusgalieta@hotmail.com>
2019-04-15 14:31:37 -0300

    Commit for CRUD

---
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Mailer{
+
+    public function __contruct($toAddress, $toName, $subject, $tplName, $data = array())
+    {
+        
+    }
+
+
+
+}
+
+
+
+?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -8,6 +8,7 @@ use \Hcode\Model;
 class User extends Model {
 
     const SESSION = "User";
+    const SECRET = "HcodePhp7_Secret";
 
     public static function login($login, $password)
     {
@@ -69,6 +70,113 @@ class User extends Model {
 
     }
 
+    public static function listAll()
+    {
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+
+    }
+
+    public function save()
+    {
+
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":desperson" => $this->getdesperson(),
+            ":deslogin" => $this->getdeslogin(),
+            ":despassword" => $this->getdespassword(),
+            ":desemail" => $this->getdesemail(),
+            ":nrphone" => $this->getnrphone(),
+            ":inadmin" => $this->getinadmin()
+        ));
+
+        $this->setData($results[0]);
+
+    }
+
+    public function get($iduser)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+            ":iduser" => $iduser
+        ));
+
+        $this->setData($results[0]);
+    }
+
+    public function update()
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":iduser" => $this->getiduser(),
+            ":desperson" => $this->getdesperson(),
+            ":deslogin" => $this->getdeslogin(),
+            ":despassword" => $this->getdespassword(),
+            ":desemail" => $this->getdesemail(),
+            ":nrphone" => $this->getnrphone(),
+            ":inadmin" => $this->getinadmin()
+        ));
+
+        $this->setData($results[0]);
+    }
+
+    public function delete()
+    {
+
+        $sql = new Sql();
+
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser" => $this->getiduser()
+        ));
+    }
+
+    public static function getForgot($email)
+    {
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+            SELECT *
+            FROM tb_persons a
+            INNER JOIN tb_users b USING(idperson)
+            WHERE a.desemail = :email;
+        ", array(
+            ":email" => $email
+        ));
+
+        if (count($results) === 0)
+        {
+            throw new \Exception("Não foi possível recuperar a senha.");    
+        }
+        else{
+            
+            $data = $results[0];
+            
+            $results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:id, :desip)", array(
+                ":iduser" => $data["iduser"],
+                ":desip" => $_SERVER["REMOTE_ADDR"]
+            ));
+
+            if (count($results2) ===0)
+            {
+                throw new \Exception("Não foi possível recuperar a senha.");
+            }
+            else{
+
+                $dataRecovery = $results2[0];
+
+                $code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovrey"], MCRYPT_MODE_ECB));
+            
+                $link = "http://localhos:8000/admin/forgot/reset?code=$code";
+            }
+        }
+    }
 
 }
 
