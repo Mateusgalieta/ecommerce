e13682798f10f862696057c8ed8d4c313f9065d3
Mateus Galieta <mateusgalieta@hotmail.com>
2019-05-06 16:59:46 -0300

    commit for my profile

--- site.php
+++ site.php
@@ -303,6 +303,64 @@ $app->post("/forgot/reset", function() {
 
   $page->setTpl("forgot-reset-sucess");
 
+});
+
+$app->get("/profile", function() {
+
+  User::verifyLogin(false);
+
+  $user = User::getFromSession();
+
+  $page = new Page();
+
+  $page->setTpl("profile", [
+    'user' => $user->getValues(),
+    'profileMsg' => User::getSuccess(),
+    'profileError' => User::getError()
+  ]);
+
+});
+
+$app->post("/profile", function() {
+
+  User::verifyLogin(false);
+
+  if(!isset($_POST['desperson']) || $_POST['desperson'] === '') {
+    User::setError("Preencha o seu nome.");
+    
+  }
+
+  if(!isset($_POST['desemail']) || $_POST['desemail'] === '') {
+    User::setError("Preencha o seu E-mail.");
+    header('Location: /profile');
+    exit;
+  }
+
+  $user = User::getFromSession();
+
+  if($_POST['desemail'] !== $user->getdesemail()) {
+
+    if(User::checkLoginExist($_POST['desemail']) === true) {
+      User::setError("Este endereço de e-mail já está cadastrado.");
+      header('Location: /profile');
+      exit;
+    }
+
+  }
+
+
+  $_POST['inadmin'] = $user->getinadmin();
+  $_POST['despassord'] = $user->getdespassword();
+  $_POST['deslogin'] = $_POST['desemail'];
+
+  $user->setData($_POST);
+
+  $user->update();
+
+  $user::setSuccess("Dados alterados com sucesso!");
+
+  header('Location: /profile');
+  exit;
 
 });
 
