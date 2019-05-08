e2f9d7b1892374fcb921a9bc0831368aea9d3666
Mateus Galieta <mateusgalieta@hotmail.com>
2019-05-08 14:02:03 -0300

    ajust table address

---
+++ views-cache/categories-menu.3fcd3edc48793c03564cb66063d67021.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/3">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/6">Samsung</a></li><li><a href="/categories/7">teste</a></li><li><a href="/categories/5">Xiaomi</a></li>
